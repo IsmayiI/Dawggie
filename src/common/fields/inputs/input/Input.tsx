bd14d830f8eb91dc133c6baefe288c356864c57b
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-06 18:35:11 +0400

    add svg components

--- src/common/fields/inputs/input/Input.tsx
+++ src/common/fields/inputs/input/Input.tsx
@@ -1,12 +1,7 @@
-import styles from './Input.module.css'
-import { InputHTMLAttributes, useRef, useState } from "react"
+import styles from '../Input.module.css'
+import { useRef, useState } from "react"
 
 
-interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'placeholder'> {
-   label: string,
-   isError?: boolean,
-   helperText?: string
-}
 
 export const Input = ({ isError = false, helperText, label, ...props }: InputProps) => {
    const [isFocus, setIsFocus] = useState(!!props.value ?? false)
@@ -14,14 +9,16 @@ export const Input = ({ isError = false, helperText, label, ...props }: InputPro
    const inputRef = useRef<HTMLInputElement>(null)
 
    return (
-      <div className={`${styles.container} ${isError ? styles.container : ''} ${isFocus ? styles.focused : ''}`}
-         onClick={() => {
-            inputRef.current?.focus()
-            setIsFocus(true)
-         }}>
-         <label className={styles.label}>{label}</label>
-         <input ref={inputRef} onBlur={() => setIsFocus(false)} className={styles.input} {...props} />
-         {/* {isError && helperText && <div className={styles.helperText}>{helperText}</div>} */}
-      </div>
+      <>
+         <div className={`${styles.container} ${isError ? styles.container : ''} ${isFocus ? styles.focused : ''}`}
+            onClick={() => {
+               inputRef.current?.focus()
+               setIsFocus(true)
+            }}>
+            <label className={styles.label}>{label}</label>
+            <input ref={inputRef} onBlur={() => !props.value && setIsFocus(false)} className={styles.input} {...props} />
+         </div>
+         {isError && helperText && <div className={styles.helperText}>{helperText}</div>}
+      </>
    )
 }
