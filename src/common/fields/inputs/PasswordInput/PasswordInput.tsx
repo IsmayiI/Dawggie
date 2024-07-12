0914ce5756461eede7844af43508667fa8853092
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-12 19:31:13 +0400

    fix inputs

--- src/common/fields/inputs/PasswordInput/PasswordInput.tsx
+++ src/common/fields/inputs/PasswordInput/PasswordInput.tsx
@@ -1,14 +1,12 @@
 import styles from '../Input.module.css'
 import passwordStyles from './PasswordInput.module.css'
-import { useRef, useState } from "react"
+import { useState } from "react"
 import { CloseSVG, ShowSVG } from '@images'
 
 
 export const PasswordInput = ({ isError = false, helperText, label, ...props }: InputProps) => {
    const [showPassword, setShowPassword] = useState(false)
-   const [isFocus, setIsFocus] = useState(!!props.value ?? false)
 
-   const inputRef = useRef<HTMLInputElement>(null)
 
    const showPasswordToggle = props.value
 
@@ -19,13 +17,16 @@ export const PasswordInput = ({ isError = false, helperText, label, ...props }: 
 
    return (
       <>
-         <div className={`${styles.container} ${isError ? styles.container : ''} ${isFocus ? styles.focused : ''}`}
-            onClick={() => {
-               inputRef.current?.focus()
-               setIsFocus(true)
-            }}>
-            <label className={styles.label}>{label}</label>
-            <input ref={inputRef} onBlur={() => !props.value && setIsFocus(false)} className={inputStyle} type={inputType} {...props} />
+         <div className={`${styles.container} ${isError ? styles.container : ''}`}>
+            <input
+               id='password'
+               placeholder=''
+               className={inputStyle}
+               type={inputType}
+               {...props} />
+            <label htmlFor="password" className={styles.label}>
+               {label}
+            </label>
             {
                showPasswordToggle &&
                <div className={passwordStyles.passwordContainer}
--- src/common/fields/inputs/input/Input.tsx
+++ src/common/fields/inputs/input/Input.tsx
@@ -1,24 +1,19 @@
 import styles from '../Input.module.css'
-import { useRef, useState } from "react"
 
 
 
-export const Input = ({ isError = false, helperText, label, ...props }: InputProps) => {
-   const [isFocus, setIsFocus] = useState(!!props.value ?? false)
-
-   const inputRef = useRef<HTMLInputElement>(null)
-
-   return (
-      <>
-         <div className={`${styles.container} ${isError ? styles.container : ''} ${isFocus ? styles.focused : ''}`}
-            onClick={() => {
-               inputRef.current?.focus()
-               setIsFocus(true)
-            }}>
-            <label className={styles.label}>{label}</label>
-            <input ref={inputRef} onBlur={() => !props.value && setIsFocus(false)} className={styles.input} {...props} />
-         </div>
-         {isError && helperText && <div className={styles.helperText}>{helperText}</div>}
-      </>
-   )
-}
+export const Input = ({ isError = false, helperText, label, ...props }: InputProps) => (
+   <>
+      <div className={`${styles.container} ${isError ? styles.container : ''}`}>
+         <input
+            id='username'
+            placeholder=''
+            className={styles.input}
+            {...props} />
+         <label htmlFor="username" className={styles.label}>
+            {label}
+         </label>
+      </div>
+      {isError && helperText && <div className={styles.helperText}>{helperText}</div>}
+   </>
+)
